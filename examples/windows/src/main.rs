404bdf67b28b5b5de42d42cda5bcca574a266cea
Edwin Svensson <git@olback.net>
2022-02-19 00:33:14 +0100

    proper windows example

--- examples/windows/src/main.rs
+++ examples/windows/src/main.rs
@@ -1,22 +1,20 @@
 use tray_item::TrayItem;
 
 fn main() {
-
-    let mut tray = TrayItem::new("Tray Example", "name-of-icon-in-rc-file").unwrap();
+    let mut tray = TrayItem::new("Tray Example", "my-icon-name").unwrap();
 
     tray.add_label("Tray Label").unwrap();
 
     tray.add_menu_item("Hello", || {
         println!("Hello!");
-    }).unwrap();
+    })
+    .unwrap();
 
     tray.add_menu_item("Quit", || {
         println!("Quit");
         std::process::exit(0);
-    }).unwrap();
-
-    tray.set_icon("another-name-from-rc-file").unwrap();
+    })
+    .unwrap();
 
     std::io::stdin().read_line(&mut String::new()).unwrap();
-
 }
